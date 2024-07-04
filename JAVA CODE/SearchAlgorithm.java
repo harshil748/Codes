6c049b9bb89f71985a0dca24110e9762630d71c2
Harshil Patel <pharshil748@gmail.com>
2024-07-04 13:27:53 +0530

    formatting changes

--- JAVA CODE/SearchAlgorithm.java
+++ JAVA CODE/SearchAlgorithm.java
@@ -2,10 +2,9 @@ import java.util.Scanner;
 import java.util.Arrays;
 
 public class SearchAlgorithm {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Scanner linear = new Scanner(System.in);
-        int size,key;
+        int size, key;
         System.out.print("Enter size of the array: ");
         size = linear.nextInt();
         int[] arr = new int[size];
@@ -22,18 +21,19 @@ public class SearchAlgorithm {
         key = linear.nextInt();
         linear.close();
         int linearIndex = linearSearch(arr, key);
-        if (linearIndex != -1){
+        if (linearIndex != -1) {
             System.out.println("Element found at index (for linear search): " + linearIndex);
         } else {
             System.out.println("Element not found (for linear search)");
         }
         int binaryIndex = binarySearch(arr, key);
-        if (binaryIndex != -1){
+        if (binaryIndex != -1) {
             System.out.println("Element found at index (for binary search): " + binaryIndex);
         } else {
             System.out.println("Element not found (for binary search)");
         }
     }
+
     public static int linearSearch(int[] arr, int key) {
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == key) {
@@ -42,6 +42,7 @@ public class SearchAlgorithm {
         }
         return -1;
     }
+
     public static int binarySearch(int[] arr, int key) {
         int left = 0, right = arr.length - 1;
         while (left <= right) {
