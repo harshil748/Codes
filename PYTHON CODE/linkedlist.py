33cf3f4e354324cdc2c3cf2441d0de2bc2c7f19e
Harshil Patel <pharshil748@gmail.com>
2024-07-18 13:16:59 +0530

    removed comments

--- PYTHON CODE/linkedlist.py
+++ PYTHON CODE/linkedlist.py
@@ -1,51 +1,47 @@
 class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
+    def __init__(ll, data):
+        ll.data = data
+        ll.next = None
 
 class LinkedList:
-    def __init__(self):
-        self.head = None
+    def __init__(ll):
+        ll.head = None
 
-    def insert_at_front(self, data):
+    def insert_at_front(ll, data):
         new_node = Node(data)
-        new_node.next = self.head
-        self.head = new_node
-        self.display()
+        new_node.next = ll.head # type: ignore
+        ll.head = new_node
+        ll.display()
 
-    def delete_at_last(self):
-        if self.head is None:
+    def delete_at_last(ll):
+        if ll.head is None:
             return
-        elif self.head.next is None:
-            self.head = None
+        elif ll.head.next is None:
+            ll.head = None
         else:
-            temp = self.head
-            while temp.next.next is not None:
-                temp = temp.next
-            temp.next = None
-        self.display()
+            temp = ll.head
+            while temp.next.next is not None: # type: ignore
+                temp = temp.next # type: ignore
+            temp.next = None # type: ignore
+        ll.display()
 
-    def delete_all(self):
-        self.head = None
-        self.display()
+    def delete_all(ll):
+        ll.head = None
+        ll.display()
 
-    def display(self):
-        if self.head is None:
+    def display(ll):
+        if ll.head is None:
             print("Linked list is empty")
         else:
-            temp = self.head
+            temp = ll.head
             while temp is not None:
                 print(temp.data, end=" ")
                 temp = temp.next
             print()
-
-# Test the implementation
 linked_list = LinkedList()
-
 linked_list.insert_at_front(3)
 linked_list.insert_at_front(2)
 linked_list.insert_at_front(1)
-
 linked_list.delete_at_last()
-
+linked_list.delete_at_last()
 linked_list.delete_all()
