8ba023b5ee93815c87b164a85e9ea422af036fce
Harshil Patel <pharshil748@gmail.com>
2024-07-18 15:46:53 +0530

    file added for practice

---
+++ JAVA CODE/Practice.java
@@ -0,0 +1,9 @@
+public class Practice {
+
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer("Java");
+        sb.setCharAt(0, 'L');
+        System.out.println(sb);
+    }
+
+}
