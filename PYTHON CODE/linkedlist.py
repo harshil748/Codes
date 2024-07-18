f1792889e840a91966518e2f8db9aa4e3b9f4630
Harshil Patel <pharshil748@gmail.com>
2024-07-18 14:07:24 +0530

    DSA Practical 4 added

--- PYTHON CODE/linkedlist.py
+++ PYTHON CODE/linkedlist.py
@@ -1,47 +1,49 @@
 class Node:
-    def __init__(ll, data):
-        ll.data = data
-        ll.next = None
+    def __init__(llist, data):
+        llist.data = data
+        llist.next = None
+
 
 class LinkedList:
-    def __init__(ll):
-        ll.head = None
+    def __init__(llist):
+        llist.head = None
 
-    def insert_at_front(ll, data):
+    def insertatfirst(llist, data):
         new_node = Node(data)
-        new_node.next = ll.head # type: ignore
-        ll.head = new_node
-        ll.display()
+        new_node.next = llist.head 
+        llist.head = new_node
+        llist.display()
 
-    def delete_at_last(ll):
-        if ll.head is None:
+    def deleteatlast(llist):
+        if llist.head is None:
             return
-        elif ll.head.next is None:
-            ll.head = None
-        else:
-            temp = ll.head
-            while temp.next.next is not None: # type: ignore
-                temp = temp.next # type: ignore
-            temp.next = None # type: ignore
-        ll.display()
+        if llist.head.next is None:
+            llist.head = None
+            return
+        temp = llist.head
+        while temp.next.next is not None:
+            temp = temp.next
+        temp.next = None
+        llist.display()
 
-    def delete_all(ll):
-        ll.head = None
-        ll.display()
+    def deleteall(llist):
+        llist.head = None
+        llist.display()
 
-    def display(ll):
-        if ll.head is None:
-            print("Linked list is empty")
+    def display(llist):
+        if llist.head is None:
+            print("List is empty")
         else:
-            temp = ll.head
+            temp = llist.head
             while temp is not None:
                 print(temp.data, end=" ")
                 temp = temp.next
             print()
+
+
 linked_list = LinkedList()
-linked_list.insert_at_front(3)
-linked_list.insert_at_front(2)
-linked_list.insert_at_front(1)
-linked_list.delete_at_last()
-linked_list.delete_at_last()
-linked_list.delete_all()
+linked_list.insertatfirst(3) 
+linked_list.insertatfirst(2)
+linked_list.insertatfirst(1)
+linked_list.deleteatlast()
+linked_list.deleteall()
