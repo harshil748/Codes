891bae6e9c8d246da5d90cc99855f49aa083efc1
Harshil Patel <pharshil748@gmail.com>
2024-07-20 13:32:47 +0530

    Practical 1.3 (UE) added

---
+++ PYTHON CODE/var.py
@@ -0,0 +1,8 @@
+university = "CHARUSAT"
+establish_in = 2009
+location_lat = 22.5996
+location_log = 72.8205
+print(type(university))
+print(type(establish_in))
+print(type(location_lat))
+print(type(location_log))
