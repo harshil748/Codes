c0511549f643945e5b8e8578870bf35d960d9604
Harshil Patel <pharshil748@gmail.com>
2024-07-20 13:42:01 +0530

    Practical 1.4 (UE) added

---
+++ PYTHON CODE/varout.py
@@ -0,0 +1,9 @@
+university = "CHARUSAT"
+establish_in = 2009
+location_lat = 22.5996
+location_log = 72.8205
+print("I am a student of " + university)
+print(university + " was established in " + str(establish_in))
+print(university + " is located on " )
+print("longitude " + str(location_log))
+print("latitude " + str(location_lat))
