0a642303663c3a916e07f72f8fb3afc303a86b98
Harshil Patel <pharshil748@gmail.com>
2024-07-20 14:11:21 +0530

    Practical 1.6 (UE) added

---
+++ PYTHON CODE/intype.py
@@ -0,0 +1,5 @@
+number = input("Enter a number: ")
+if number.isdigit():
+    print("Data type of the variable: ", type(int(number)))
+else:
+    print("Data type of the variable: ", type(float(number)))
