18d4464f114d0e48986954a3b9c710fe434f78e6
Harshil Patel <pharshil748@gmail.com>
2024-07-20 14:16:18 +0530

    Practical 1.7 (UE) added

--- PYTHON CODE/calc.py
+++ PYTHON CODE/calc.py
@@ -1,17 +1,24 @@
-def add(n1,n2):
-    return n1+n2
-def subtract(n1,n2):
-    return n1-n2
-def multiply(n1,n2):
-    return n1*n2
-def divide(n1,n2):
-    if n2!=0:
-        return n1/n2
-    else:
-        return "Division by zero is not possible."
-n1 = float(input("Enter the first number: "))
-n2 = float(input("Enter the second number: "))
-print("addition of numbers is: ",add(n1,n2))
-print("substraction of numbers is: ",subtract(n1,n2))
-print("multipliction of numbers is: ",multiply(n1,n2))
-print("division of numbers is: ",divide(n1,n2))
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+addition = num1 + num2
+subtraction = num1 - num2
+division = num1 / num2
+multiplication = num1 * num2
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Division:", division)
+print("Multiplication:", multiplication)
+
+if addition == int(addition):
+    addition = int(addition)
+if subtraction == int(subtraction):
+    subtraction = int(subtraction)
+if division == int(division):
+    division = int(division)
+if multiplication == int(multiplication):
+    multiplication = int(multiplication)
+    
+print("Data type of addition:", type(addition))
+print("Data type of subtraction:", type(subtraction))
+print("Data type of division:", type(division))
+print("Data type of multiplication:", type(multiplication))
