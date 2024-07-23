a3f915b76ced76efd9748c768764270112ec086d
Harshil Patel <pharshil748@gmail.com>
2024-07-23 15:47:39 +0530

    Practical 1.8 (UE) added

--- PYTHON CODE/deporeturn.py
+++ PYTHON CODE/deporeturn.py
@@ -1,6 +1,5 @@
 principal = float(input("Enter the fixed deposit amount: "))
 rate = float(input("Enter the interest rate: "))
 time = float(input("Enter the tenure in years: "))
-simple_interest = (principal * rate * time) / 100
 total_return = principal + simple_interest
 print("The total return after", time, "years is:", total_return)
