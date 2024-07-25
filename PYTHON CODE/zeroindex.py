a701870f6508d67c25dc7b930214b69a9635e885
Harshil Patel <pharshil748@gmail.com>
2024-07-25 13:59:27 +0530

    DSA Practical 2.3 added

---
+++ PYTHON CODE/zeroindex.py
@@ -0,0 +1,20 @@
+def rearrange_array(arr):
+    n = len(arr)
+    for i in range(1, n, 2):
+    return
+t = int(input())
+'''
+# Iterate over each test case
+for _ in range(t):
+    # Read the size of the array
+    n = int(input())
+    
+    # Read the elements of the array
+    arr = list(map(int, input().split()))
+    
+    # Rearrange the array
+    rearranged_arr = rearrange_array(arr)
+    
+    # Print the rearranged array
+    print(*rearranged_arr)
+''' 
