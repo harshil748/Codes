5a0b2c69ef94fba382d93210f3739b0a31069108
Harshil Patel <pharshil748@gmail.com>
2024-07-29 15:03:00 +0530

    Java Practical 5 added

---
+++ JAVA CODE/Anagrams.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Random;
+public class Anagrams {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first string: ");
+        String str1 = sc.nextLine();
+        System.out.println("Enter the second string: ");
+        String str2 = sc.nextLine();
+        if (isAnagram(str1, str2)) {
+            System.out.println("The strings are anagrams.");
+        } else {
+            System.out.println("The strings are not anagrams.");
+        }
+    }
+}
