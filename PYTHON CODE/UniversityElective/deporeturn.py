76774af0f2ff94ecdbb8395156871f8755f5a2e6
Harshil Patel <pharshil748@gmail.com>
2024-07-30 14:45:12 +0530

    # Practical 1.8

--- PYTHON CODE/UniversityElective/deporeturn.py
+++ PYTHON CODE/UniversityElective/deporeturn.py
@@ -3,3 +3,4 @@ rate = float(input("Enter the interest rate: "))
 time = float(input("Enter the tenure in years: "))
 total_return = principal + simple_interest
 print("The total return after", time, "years is:", total_return)
+# Practical 1.8
