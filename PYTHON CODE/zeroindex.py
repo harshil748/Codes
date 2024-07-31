fc72332fa7515fa56f7345a5f3e519953916b085
Harshil Patel <pharshil748@gmail.com>
2024-07-31 16:16:12 +0530

    code changes

--- PYTHON CODE/zeroindex.py
+++ PYTHON CODE/zeroindex.py
@@ -1,20 +1,17 @@
-def rearrange_array(arr):
-    n = len(arr)
-    for i in range(1, n, 2):
-    return
-t = int(input())
-'''
-# Iterate over each test case
-for _ in range(t):
-    # Read the size of the array
-    n = int(input())
+def zero(arr):
+    # Sort the array (bubble sort)
+    for i in range(len(arr)):
+        for j in range(len(arr) - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
     
-    # Read the elements of the array
-    arr = list(map(int, input().split()))
+    # Reorder elements
+    for i in range(1, len(arr) - 1, 2):
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
     
-    # Rearrange the array
-    rearranged_arr = rearrange_array(arr)
-    
-    # Print the rearranged array
-    print(*rearranged_arr)
-''' 
+    # Print the result
+    print(' '.join(str(num) for num in arr))
+
+# Test the function
+arr = [1, 5, 3, 7, 8, 2, 4, 6]
+zero(arr)
