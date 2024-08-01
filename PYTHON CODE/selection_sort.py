590e6950c2b5f0c45c65fa28dcbea90076bb007b
Harshil Patel <pharshil748@gmail.com>
2024-08-01 12:26:35 +0530

    comments added

--- PYTHON CODE/selection_sort.py
+++ PYTHON CODE/selection_sort.py
@@ -1,14 +1,14 @@
-def selection_sort(array):
-    n = len(array)
-    for i in range(n):
-        min_idx = i
-        for j in range(i+1, n):
-            if array[j] < array[min_idx]:
-                min_idx = j
-        temp = array[min_idx]
-        array[min_idx] = array[i]
-        array[i] = temp
-    return array
+def selection_sort(array):                  # 
+    n = len(array)                          # calculate the length of the array
+    for i in range(n):                      # iterate through the array
+        min_idx = i                         # set the minimum index to the current index
+        for j in range(i+1, n):             # iterate through the array starting from the next index
+            if array[j] < array[min_idx]:   # if the current element is less than the minimum element
+                min_idx = j                 # set the minimum index to the current index
+        temp = array[min_idx]               # swap the minimum element with the current element
+        array[min_idx] = array[i]           # swap the minimum element with the current element
+        array[i] = temp                     # swap the minimum element with the current element
+    return array                            # return the sorted array
 
 array = [64, 25, 12, 22, 11]
 selection_sort(array)
