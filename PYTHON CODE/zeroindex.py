50afdbe548f399c7f4dcc40bbb57d9881b301e41
Harshil Patel <pharshil748@gmail.com>
2024-08-01 12:56:56 +0530

    DSA Practical Part 2.3

--- PYTHON CODE/zeroindex.py
+++ PYTHON CODE/zeroindex.py
@@ -1,17 +1,12 @@
-def zero(arr):
-    # Sort the array (bubble sort)
-    for i in range(len(arr)):
-        for j in range(len(arr) - 1):
-            if arr[j] > arr[j + 1]:
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
-    
-    # Reorder elements
-    for i in range(1, len(arr) - 1, 2):
-        arr[i], arr[i + 1] = arr[i + 1], arr[i]
-    
-    # Print the result
-    print(' '.join(str(num) for num in arr))
-
-# Test the function
-arr = [1, 5, 3, 7, 8, 2, 4, 6]
-zero(arr)
+array = [1, 2, 3, 4, 5, 6]
+def zero(array):
+    for i in range(len(array) - 1):
+        for j in range(len(array) - 1):
+            if array[j] > array[j + 1]:
+                array[j], array[j + 1] = array[j + 1], array[j]
+                
+        for j in range(1, len(array) - 1, 2):
+            if array[j] < array[j + 1]:
+                array[j], array[j + 1] = array[j + 1], array[j]
+zero(array)
+print(array)
