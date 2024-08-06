b703001e193bd23bbab1d97627fe20a2debfc7ca
Harshil Patel <pharshil748@gmail.com>
2024-08-06 10:56:47 +0530

    code changes

--- JAVA CODE/Point.java
+++ JAVA CODE/Point.java
@@ -7,12 +7,12 @@ public class Point {
         this.y = stay(y, 0, 100);
     }
 
-    public String toString() { 
+    public String toString() {
         return "(" + x + ", " + y + ")";
     }
 
-    public void move(int dx, int dy) { 
-        x = stay(x + dx, 0, 100); 
+    public void move(int dx, int dy) {
+        x = stay(x + dx, 0, 100);
         y = stay(y + dy, 0, 100);
     }
 
@@ -20,18 +20,16 @@ public class Point {
         return Math.max(min, Math.min(value, max));
     }
 
-    public class PointTest {
-        public static void main(String[] args) {
-            Point p1 = new Point(50, 50);
-            System.out.println(p1);
-            p1.move(30, 40);
-            System.out.println(p1);
-            p1.move(-90, 20);
-            System.out.println(p1);
-            p1.move(10, -150);
-            System.out.println(p1);
-            p1.move(200, 200);
-            System.out.println(p1);
-        }
+    public static void main(String[] args) {
+        Point p1 = new Point(50, 50);
+        System.out.println(p1);
+        p1.move(30, 40);
+        System.out.println(p1);
+        p1.move(-90, 20);
+        System.out.println(p1);
+        p1.move(10, -150);
+        System.out.println(p1);
+        p1.move(200, 200);
+        System.out.println(p1);
     }
 }
