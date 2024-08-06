980d20113b7142beccf2825abee6ced03cbf124d
Harshil Patel <pharshil748@gmail.com>
2024-08-06 11:10:12 +0530

    Java Practical 9 added

--- JAVA CODE/GasMeter.java
+++ JAVA CODE/GasMeter.java
@@ -19,4 +19,34 @@ public class GasMeter {
                 break;
         }
     }
+    public static double total() {
+        return octane95 + octane98 + diesel;
+    }
+    public static double total95() {
+        return octane95;
+    }
+    public static double total98() {
+        return octane98;
+    }
+    public static double totalD() {
+        return diesel;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(true){
+            System.out.println("what do you want: 1=95, 2=98, 3=Diesel (type any other number to quit): ");
+            int choice = sc.nextInt();
+            if (choice < 1 || choice > 3) {
+                break;
+            }
+            System.out.println("How much do you want to refuel: ");
+            String amount = sc.next();
+            refuel(choice, amount);
+        }
+        sc.close();
+        System.out.println("Total refueled: " + total());
+        System.out.println("Total refueled 95 Octane: " + total95());
+        System.out.println("Total refueled 98 Octane: " + total98());
+        System.out.println("Total refueled Diesel: " + totalD());
+    }
 }
