7d54f696490085bb163762e40be8a4a86ecb0d37
Harshil Patel <pharshil748@gmail.com>
2024-08-13 10:39:05 +0530

    formatting

--- JAVA CODE/Harmonic.java
+++ JAVA CODE/Harmonic.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 import harmonic.HarmonicSeries;
+
 public class Harmonic {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
