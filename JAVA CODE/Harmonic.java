5cbe75bad8327e6fac14120b58929135d64981c5
Harshil Patel <pharshil748@gmail.com>
2024-08-13 10:02:29 +0530

    JAVA Practical 14

---
+++ JAVA CODE/Harmonic.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import harmonic.HarmonicSeries;
+public class Harmonic {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the value of n: ");
+        int n = scanner.nextInt();
+        HarmonicSeries harmonicSeries = new HarmonicSeries();
+        double sum = harmonicSeries.calculate(n);
+        System.out.println("Sum of the harmonic series is: " + sum);
+        scanner.close();
+    }
+}
---
+++ JAVA CODE/harmonic/HarmonicSeries.java
@@ -0,0 +1,10 @@
+package harmonic;
+public class HarmonicSeries {
+    public double calculate(int n) {
+        double sum = 0.0;
+        for (int i = 1; i <= n; i++) {
+            sum += 1.0 / i;
+        }
+        return sum;
+    }
+}
