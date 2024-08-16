0478926c085fbab00973b5cfa36eb4c7fe7a9807
Harshil Patel <pharshil748@gmail.com>
2024-08-16 16:22:30 +0530

    # Practical 2.3

---
+++ PYTHON CODE/UniversityElective/numbtw.py
@@ -0,0 +1,6 @@
+num = int(input("Enter a number: "))
+
+if num > 10 and num < 20:
+    print(f"The number {num} is between 10 and 20.")
+else:
+    print(f"The number {num} is not between 10 and 20.")
