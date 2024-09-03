1e0f5da4b0804498e89e2e5587fcadb123ce2bb9
Harshil Patel <pharshil748@gmail.com>
2024-09-03 10:06:05 +0530

    Java Practical 6.1 added

---
+++ JAVA CODE/Threading.java
@@ -0,0 +1,23 @@
+class MyThread extends Thread {
+    public void run() {
+        System.out.println("Printing by extending Thread class");
+        System.out.println("Hello World");
+    }
+}
+
+class MyRunnable implements Runnable {
+    public void run() {
+        System.out.println("Printing by using Runnable interface");
+        System.out.println("Hello World");
+    }
+}
+
+public class Threading {
+    public static void main(String[] args) {
+        MyThread t1 = new MyThread();
+        t1.start();
+        MyRunnable r = new MyRunnable();
+        Thread t2 = new Thread(r);
+        t2.start();
+    }
+}
