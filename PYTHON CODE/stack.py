ec57dbbba8c590999964285d8e7432348a540248
Harshil Patel <pharshil748@gmail.com>
2024-09-11 15:26:31 +0530

    code changes

--- PYTHON CODE/stack.py
+++ PYTHON CODE/stack.py
@@ -1,40 +1,31 @@
 class Stack:
-    def __init__(self):
-        self.stack = [] 
-        
-    def push(self, item):
-        self.stack.append(item)
-        
-    def pop(self):
-        if self.is_empty():
-            return None
-        return self.stack.pop()
-    
-    def is_empty(self):
-        return len(self.stack) == 0
-    
-    def size(self):
-        return len(self.stack)
-    
-    def isvalid(string : str) -> bool:
+    def isValid(self, string: str) -> bool:
         stack = []
         for char in string:
-            if char in ['(', '[', '{']:
+            if char in ["(", "[", "{"]:
                 stack.append(char)
             else:
                 if not stack:
                     return False
                 current_char = stack.pop()
-                if current_char == '(':
-                    if char != ')':
+                if current_char == "(":
+                    if char != ")":
                         return False
-                if current_char == '[':
-                    if char != ']':
+                if current_char == "[":
+                    if char != "]":
                         return False
-                if current_char == '{':
-                    if char != '}':
+                if current_char == "{":
+                    if char != "}":
                         return False
         return len(stack) == 0
-    
+
 stack = Stack()
-print(isvalid("([)])))"))
+print("( Char is: "+ str(stack.isValid("(")))
+print(") Char is: " + str(stack.isValid(")")))
+print("[ Char is: " + str(stack.isValid("[")))
+print("] Char is: " + str(stack.isValid("]")))
+print("{ Char is: " + str(stack.isValid("{")))
+print("} Char is: " + str(stack.isValid("}")))
+print("() Char is: " + str(stack.isValid("()")))
+print("[] Char is: " + str(stack.isValid("[]")))
+print("{} Char is: " + str(stack.isValid("{}")))
