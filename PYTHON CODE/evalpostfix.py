09d37f83f966524ec8218fe7069e6e76b3f18265
Harshil Patel <pharshil748@gmail.com>
2024-09-19 13:11:28 +0530

    DSA Practical 5.3(c)

---
+++ PYTHON CODE/evalpostfix.py
@@ -0,0 +1,22 @@
+def evaluate_postfix(expression):
+    stack = []
+    for char in expression.split():
+        if char.isdigit():
+            stack.append(int(char))
+        else:
+            operand2 = stack.pop()
+            operand1 = stack.pop()
+            if char == "+":
+                stack.append(operand1 + operand2)
+            elif char == "-":
+                stack.append(operand1 - operand2)
+            elif char == "*":
+                stack.append(operand1 * operand2)
+            elif char == "/":
+                stack.append(operand1 / operand2)
+    return stack[0]
+
+expression = input("Enter the postfix expression: ")
+print("The entered post expression is"+expression)
+result = evaluate_postfix(expression)
+print(result)
