ea11f765a48f9ed53c8aa5c8eff9d2b7e4fe76f8
Harshil Patel <pharshil748@gmail.com>
2024-09-24 10:12:41 +0530

    code changes

--- JAVA CODE/FileHandling.java
+++ JAVA CODE/FileHandling.java
@@ -1,10 +1,18 @@
-import java.io.File;
-import java.io.IOException;
-
+import java.io.*;
+import java.util.Scanner;
 public class FileHandling {
     public static void main(String[] args) {
         try {
             File obj = new File("filename.txt");
+            FileWriter wr = new FileWriter("filename.txt");
+            Scanner sc = new Scanner(System.in);
+            FileReader fr = new FileReader("filename.txt");
+            wr.write("Successfully written to the file.");
+            wr.close();
+            while (sc.hasNextLine()) {
+                String str = sc.nextLine();
+                wr.write(str);
+            }
             if (obj.createNewFile()) {
                 System.out.println("File created: " + obj.getName());
             } else {
