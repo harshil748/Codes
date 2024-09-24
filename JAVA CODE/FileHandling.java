23bb5b9a363f21e1a9a95add4251c4bd27c6e915
Harshil Patel <pharshil748@gmail.com>
2024-09-24 10:22:41 +0530

    added more methods

--- JAVA CODE/FileHandling.java
+++ JAVA CODE/FileHandling.java
@@ -1,23 +1,21 @@
 import java.io.*;
-import java.util.Scanner;
 public class FileHandling {
     public static void main(String[] args) {
         try {
             File obj = new File("filename.txt");
             FileWriter wr = new FileWriter("filename.txt");
-            Scanner sc = new Scanner(System.in);
-            FileReader fr = new FileReader("filename.txt");
             wr.write("Successfully written to the file.");
             wr.close();
-            while (sc.hasNextLine()) {
-                String str = sc.nextLine();
-                wr.write(str);
-            }
             if (obj.createNewFile()) {
                 System.out.println("File created: " + obj.getName());
             } else {
                 System.out.println("File already exists");
             }
+            System.out.println("File Name: " + obj.getName());
+            System.out.println("Absolute path: " + obj.getAbsolutePath());
+            System.out.println("File exists: " + obj.length());
+            System.out.println("File readable: " + obj.canRead());
+            System.out.println("File writable: " + obj.canWrite());
         } catch (IOException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
