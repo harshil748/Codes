c6a861895cee881a65488c2906f68587cb686ef9
Harshil Patel <pharshil748@gmail.com>
2024-09-24 10:44:50 +0530

    code changes

--- JAVA CODE/Stream.java
+++ JAVA CODE/Stream.java
@@ -3,7 +3,7 @@ public class Stream {
     public static void main(String[] args) {
         try {
             FileInputStream fis = new FileInputStream("abc.txt");
-            FileOutputStream fos = new FileOutputStream("abc.txt");
+            FileOutputStream fos = new FileOutputStream("xyz.txt");
             int c;
             while ((c = fis.read()) != -1) {
                 fos.write(c);
