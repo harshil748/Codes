73016ad43c9991e68f7dd26a2d6eefaf0bf3c65f
Harshil Patel <pharshil748@gmail.com>
2024-09-27 16:15:06 +0530

    # Practical 3.5

---
+++ PYTHON CODE/UniversityElective/prime.py
@@ -0,0 +1,23 @@
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+
+def find_n_primes(n):
+    primes = []
+    num = 2
+    while len(primes) < n:
+        if is_prime(num):
+            primes.append(num)
+        num += 1
+    return primes
+
+
+n = int(input("Enter the number of prime numbers to find: "))
+result = find_n_primes(n)
+print(f"The first {n} prime numbers are:")
+print(result)
