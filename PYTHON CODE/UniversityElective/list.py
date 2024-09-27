791b7de7ddfcf5eec3fd46d4d6d9275ab42b61ad
Harshil Patel <pharshil748@gmail.com>
2024-09-27 16:01:09 +0530

    # Practical 3.1

---
+++ PYTHON CODE/UniversityElective/list.py
@@ -0,0 +1,10 @@
+flowers = ["Rose", "Lily", "Sunflower", "Tulip", "Daisy"]
+print("a. Original list of flowers:")
+print(flowers)
+print("\nb. Second element of the list:")
+print(flowers[1])
+print("\nc. Third element and all elements after:")
+print(flowers[2:])
+flowers[1] = "Hibiscus"
+print("\nd. List after replacing the second element with 'Hibiscus':")
+print(flowers)
