6b51dc2f80fa0e9508f4dddf745a4d326336c770
Harshil Patel <pharshil748@gmail.com>
2024-12-31 14:33:37 +0530

    code changes

--- PYTHON CODE/list.py
+++ PYTHON CODE/list.py
@@ -1,12 +1,3 @@
 strings= ["a", "b", "c"]
 integers = ["1", "2", "3"]
-mixed = ["a", 1, "b", 2]    
-print("a. Original list of flowers:")
-print(flowers)
-print("\nb. Second element of the list:")
-print(flowers[1])
-print("\nc. Third element and all elements after:")
-print(flowers[2:])
-flowers[1] = "Hibiscus"
-print("\nd. List after replacing the second element with 'Hibiscus':")
-print(flowers)
+mixed = ["a", 1, "b", 2] 
