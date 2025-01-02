c17b4b09214f2f4f894751072a9516edf51e7f5e
Harshil Patel <pharshil748@gmail.com>
2025-01-02 13:42:28 +0530

    code changes

--- DAA/recfac.py
+++ DAA/recfac.py
@@ -1,9 +1,12 @@
 def factorial(n):
-    if n == 0 or n == 1:
-        return 1
+    if n == 0 or n == 1:  # 2 comparison operation
+        return 1  # 1 return operation
 
     return n * factorial(n - 1)
-
+    # 3 operations
+    # 1. multiplication
+    # 2. recursive function call
+    # 3. return result
 
 n = int(input())
 print(factorial(n))
