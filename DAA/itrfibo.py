b97a0457ba36cf0f01b3d1c325b8a71552fac40e
Harshil Patel <pharshil748@gmail.com>
2025-01-02 13:52:25 +0530

    code changes

--- DAA/itrfibo.py
+++ DAA/itrfibo.py
@@ -1,22 +1,15 @@
 def fibonacci_sequence(n):
     if n <= 0:  # input valid 1-2 comparison operations
         return []  # 1 return operation
-
-    fib_seq = [0, 1]  # Initial 2 operations
-
+    fib_seq = [0, 1]  # initial 2 operations
     while True:
         next_num = fib_seq[-1] + fib_seq[-2]  
-        # Last two element : 2 operations
-        # Addition operation: 1
-
-        if next_num > n:  # Comparison : 1 operation
+        # last two element  2 operations
+        # addition operation: 1
+        if next_num > n:  # comparison  1 operation
             break
-
-        fib_seq.append(next_num)  # Append operation: 1
-
+        fib_seq.append(next_num)  # append operation: 1
     return fib_seq
 
-
 n = int(input())
-
 print(*fibonacci_sequence(n))
