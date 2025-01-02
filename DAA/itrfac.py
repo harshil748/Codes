202e18d42ae5b8e1e6bf4fd2c82d63d58f7d603f
Harshil Patel <pharshil748@gmail.com>
2025-01-02 13:54:20 +0530

    code changes

--- DAA/itrfac.py
+++ DAA/itrfac.py
@@ -1,13 +1,10 @@
 def factorial(n):
     if n == 0 or n == 1:  # 2 comparison operation
         return 1  # 1 return operation
-
     result = 1  # 1 assignment operation
-    for i in range(2, n + 1):  # Iterations: (n-1) times
+    for i in range(2, n + 1):  # iterations (n-1) times
         result *= i  # 2 operations per iteration
-
     return result
-
 
 n = int(input())
 print(factorial(n))
