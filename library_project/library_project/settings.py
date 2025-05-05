f9aa0d47bb5524f5b9c68c3aedadad2bfd50ef42
Harshil Patel <pharshil748@gmail.com>
2025-05-05 10:54:21 +0530

    set debug to true

--- library_project/library_project/settings.py
+++ library_project/library_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-0250@z_2oc-_i1jk3r_93(k#183kfv#bb_upr%)kt*k8n+#z$c"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [ "localhost", "127.0.0.1" ]
 
