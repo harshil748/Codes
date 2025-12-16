52312c470130152b13dae3e3b3c490132dc2e449
Harshil Patel <pharshil748@gmail.com>
2025-12-16 12:14:18 +0530

    Remove unnecessary comment about lazy TensorFlow import

--- mnist_mlp/train_mnist_mlp.py
+++ mnist_mlp/train_mnist_mlp.py
@@ -7,7 +7,6 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.metrics import confusion_matrix, classification_report
 
-# Lazy import TensorFlow to allow help/--info without heavy startup
 
 
 def lazy_tf_import():
