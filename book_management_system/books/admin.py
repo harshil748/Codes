52cd7161e5e32707fa862af27260011ce8649638
Harshil Patel <pharshil748@gmail.com>
2025-12-16 12:20:54 +0530

    remove this also

--- book_management_system/books/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Book
-
-
-@admin.register(Book)
-class BookAdmin(admin.ModelAdmin):
-    list_display = ("title", "author", "publication_date", "isbn", "genre")
-    search_fields = ("title", "author", "isbn")
-    list_filter = ("genre", "publication_date")
--- calculator_project/calculator/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.calculator, name="calculator"),
-]
-# Compare this snippet from calculator_project/calculator/views.py:
