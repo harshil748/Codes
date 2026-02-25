196448f72fa57476e1462d070fecf6c3ec8aafbf
Harshil Patel <pharshil748@gmail.com>
2026-02-25 13:22:00 +0530

    Add Diffie-Hellman implementation with key generation and output

---
+++ CYBER/diffie_hellman_sha256.py
@@ -0,0 +1,22 @@
+def power(a, b, p):
+    if b == 1:
+        return a
+    else:
+        return pow(a, b) % p
+
+
+P = 23
+print("The value of P:", P)
+G = 5
+print("The value of G:", G)
+a = 4
+print("The private key a for Alice:", a)
+x = power(G, a, P)
+b = 3
+print("The private key b for Bob:", b)
+y = power(G, b, P)
+ka = power(y, a, P)
+kb = power(x, b, P)
+
+print("Secret key for Alice is:", ka)
+print("Secret key for Bob is:", kb)
